f152d2e89831c257c21146accc6f17f10ca486df
Tac Tacelosky <tacman@gmail.com>
2022-04-21 12:32:46 -0400

    bump to 2021.3 LTS and new Input System

--- Doodle Jump/Assets/Player.cs
+++ Doodle Jump/Assets/Player.cs
@@ -1,15 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 [RequireComponent(typeof(Rigidbody2D))]
 public class Player : MonoBehaviour {
 
 	public float movementSpeed = 10f;
 
+	private Vector2 inputVec = Vector2.zero;
 	Rigidbody2D rb;
 
 	float movement = 0f;
+	
+	public void OnMove(InputValue input)
+	{
+		inputVec = input.Get<Vector2>();
+		// moveVec = new Vector3(inputVec.x, 0, inputVec.y);
+	}
 
 	// Use this for initialization
 	void Start () {
@@ -18,7 +26,7 @@ public class Player : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		movement = Input.GetAxis("Horizontal") * movementSpeed;
+		movement = inputVec.x * movementSpeed;
 	}
 
 	void FixedUpdate()
